c3822aa729b14a00751b896e2320f9d2c1b4a06a
strangelights <jbrookshire7@gmail.com>
2018-08-30 15:03:49 -0400

    fixing search background and status modal

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 
+
 **A DARK THEME FOR SLACK**
 
 Append the js script to the bottom of /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js
 
-QuickTip: In Finder (Mac), press Shift + Command + D and paste in the above path into the field and press Go
+QuickTip: In Finder (Mac), press Shift + Command + G and paste in the above path into the field and press Go
 
 If sidebar customization is your thing, try sidebar colors: #363636,#444A47,#D39B46,#FFFFFF,#434745,#FFFFFF,#99D04A,#DB6668
--- slack_dark_theme.js
+++ slack_dark_theme.js
@@ -39,6 +39,72 @@ document.addEventListener('DOMContentLoaded', function() {
        div.c-message:hover {
            background-color: #222222 !important;
        }
+
+       ts-message.active:not(.standalone):not(.multi_delete_mode):not(.highlight):not(.new_reply):not(.show_broadcast_indicator), ts-message.message--focus:not(.standalone):not(.multi_delete_mode):not(.highlight):not(.new_reply):not(.show_broadcast_indicator), ts-message:hover:not(.standalone):not(.multi_delete_mode):not(.highlight):not(.new_reply):not(.show_broadcast_indicator){
+          background-color: #222222 !important;
+       }
+
+       ts-thread .view_all_replies_container:hover {
+          background-color: #222222 !important;
+      }
+
+      .c-search_autocomplete {         
+          background-color: #222222 !important;  
+      }
+
+      .c-search_autocomplete footer {
+          background-color: #222222 !important;  
+      }
+
+      .c-search__input_box {
+          background-color: #363636 !important;  
+      }
+
+      .dense_theme #reply_container.upload_in_threads .inline_message_input_container, .dense_theme #reply_container.upload_in_threads .reply_container_info, .light_theme #reply_container.upload_in_threads .inline_message_input_container, .light_theme #reply_container.upload_in_threads .reply_container_info{
+          background-color: #222222 !important;
+      }
+
+      #threads_msgs .inline_message_input_container.with_file_upload .ql-container {
+            background-color: #222222 !important;
+      }
+
+      #threads_msgs .inline_message_input_container.with_file_upload {
+               background-color: #222222 !important;
+      }
+
+      .menu ul li a:not(.inline_menu_link) {
+            color: #e6e6e6 !important;
+      }
+      .c-search_autocomplete__suggestion_item {
+           color: #e6e6e6 !important;
+      }
+      .c-search_autocomplete__suggestion_item .token {
+            color: #e6e6e6 !important;
+      }
+      .c-search__view {
+            background-color: #363636 !important;
+      }
+      .c-tabs__tab_menu {
+            background-color: #363636 !important;
+      }
+      .c-search__input_and_close {
+        background-color: #363636 !important;
+        border-bottom: 1px solid #545454; !important;
+      }
+      .c-search_message__body {
+        color: #e6e6e6 !important;
+      }
+      .p-search_filter__title_text {
+        background-color: #363636 !important;
+      }
+      .c-message_group {
+        background-color: #222222 !important;
+        border-color: #222222 !important;
+      }
+      .c-search_autocomplete header {
+          margin-top: 0px !important;
+      }
+
      `;
      $("<style></style>").appendTo('head').html(css);
    }

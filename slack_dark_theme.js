cf4a0956b9bef31de163538d76d5a9bc7c3f1ed5
strangelights <jbrookshire7@gmail.com>
2019-08-06 14:58:43 -0400

    Remove Jquery
    Replaced Jquery ajax method with plain ol' vanilla JS XMLHttpRequest() resolving issue #4.

--- slack_dark_theme.js
+++ slack_dark_theme.js
@@ -3,18 +3,23 @@
 // Everything above this line should be part of the original ssb-interop.js file DO NOT REMOVE IT.
 // Everything below is safe to edit as needed.
 
-document.addEventListener("DOMContentLoaded", function() {
-  $.ajax({
-    url: "DARK_THEME_URL",
-    success: function(css) {
-      css += ` 
-                /* Add user customizable styles here */
-            `;
-      $("<style></style>")
-        .appendTo("head")
-        .html(css);
-    }
-  });
+document.addEventListener("DOMContentLoaded", function () {
+    var darkThemeRequest = new XMLHttpRequest();
+    darkThemeRequest.open('GET', 'DARK_THEME_URL', true);
+    darkThemeRequest.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            var css = darkThemeRequest.responseText;
+            var styleTags = document.createElement('style');
+            css += `
+                    /* Add user customizable styles here */
+            `;  
+            styleTags.id = "darkTheme";
+            styleTags.type = "text/css";
+            document.getElementsByTagName('head')[0].appendChild(styleTags);
+            document.head.querySelector("#darkTheme").innerHTML = css;
+        }
+    };
+    darkThemeRequest.send();
 });
 
 /* ------------ END DARK THEME ------------ */
